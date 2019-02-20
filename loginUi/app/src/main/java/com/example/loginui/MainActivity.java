d7b900788a2c998d308d40b2b65968a45b02343e
nikhilbaliyan <nikhilbaliyan113@gmail.com>
2019-02-20 16:11:03 +0530

    Added all the files(First Commit)

---
+++ loginUi/app/src/main/java/com/example/loginui/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.loginui;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout rellay;
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay.setVisibility(View.VISIBLE);
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        rellay = (RelativeLayout) findViewById(R.id.rellay);
+
+        handler.postDelayed(runnable,2000);
+    }
+}

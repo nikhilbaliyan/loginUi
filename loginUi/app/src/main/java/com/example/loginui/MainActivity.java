112cf90852687cf50ead597fac7ca22053910456
nikhilbaliyan <nikhilbaliyan113@gmail.com>
2019-02-22 11:03:20 +0530

    this consist of landing page

--- loginUi/app/src/main/java/com/example/loginui/MainActivity.java
+++ loginUi/app/src/main/java/com/example/loginui/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.loginui;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,14 +20,29 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//
-//        rellay = (RelativeLayout) findViewById(R.id.rellay);
-//
-//        handler.postDelayed(runnable,2000);
-//    }
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        rellay = (RelativeLayout) findViewById(R.id.rellay);
+
+        handler.postDelayed(runnable,2000);
+
+
+        button = (Button) findViewById(R.id.buttonn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openlol();
+            }
+        });
+    }
+    public void openlol(){
+        Intent intent = new Intent(this,landing.class);
+        startActivity(intent);
+    }
 }
---
+++ loginUi/app/src/main/java/com/example/loginui/it.java
@@ -0,0 +1,17 @@
+package com.example.loginui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class it extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
